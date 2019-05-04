148222cf1ca4728df9487a4e346a5a26dd270a9a
Muhammad Bawaihi <46224835+waihihihi@users.noreply.github.com>
2019-05-04 15:55:38 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
  */
-$config['base_url'] = 'http://localhost/Program-KP/';
+$config['base_url'] = $_SERVER['SERVER_ADDR'];
 
 /*
 |--------------------------------------------------------------------------
